64c39ca34fe1e31c85559ed56a4adb7fa827c763
ashpring <artshpringer2006@gmail.com>
2024-10-12 21:55:29 +0500

    flags

--- cmd/triple-s/main.go
+++ cmd/triple-s/main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+var (
+	help = flag.Bool("help", false, "Show help screen")
+	port = flag.String("port", "3000", "Port number")
+	dir  = flag.String("dir", "data", "Path to the directory")
+)
+
 func main() {
+	flag.Parse()
+
+	if *help {
+		message := `Simple Storage Service.
+**Usage:**
+	triple-s [-port <N>] [-dir <S>]  
+	triple-s --help
+		
+**Options:**
+	- --help     Show this screen.
+	- --port N   Port number
+	- --dir S    Path to the directory`
+		fmt.Println(message)
+		return
+	}
+
+	// Creatin new server
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handlerMangaer)
+	mux.HandleFunc("/createbucket", handlerMangaer)
 
-	log.Print("starting server on :3000")
-	err := http.ListenAndServe(":3000", mux)
+	log.Printf("starting server on :%v\n", *port)
+	err := http.ListenAndServe(":"+*port, mux)
 	log.Fatal(err)
 }
