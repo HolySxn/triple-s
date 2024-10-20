2869e423c9be0e6e13898cdd1f3b008e43d774fc
ashpring <artshpringer2006@gmail.com>
2024-10-20 17:31:45 +0500

    object get

--- handlers/object-handler.go
+++ handlers/object-handler.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"io"
 	"net/http"
+	"os"
 	"path"
+	"strconv"
 	"time"
 
 	"triple-s/internal/object"
@@ -21,37 +23,64 @@ func ObjectHnadler(dir string) http.HandlerFunc {
 		object_name := r.PathValue("ObjectName")
 		switch r.Method {
 		case http.MethodPut:
-			data, err := io.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				return
-			}
+			ObjectPut(w, r, dir, bucket_name, object_name)
+		case http.MethodGet:
+			ObjectGet(w, r, dir, bucket_name, object_name)
+		case http.MethodDelete:
 
-			object_dir := path.Join(dir, bucket_name, object_name)
-			status := object.PutObject(data, object_dir)
-			if status != http.StatusOK {
-				http.Error(w, http.StatusText(status), status)
-				return
-			}
-
-			csv_dir := path.Join(dir, bucket_name, "objects.csv")
-			record := []string{object_name, http.DetectContentType(data[:512]), time.Now().Format("2006-01-02 15:04:05 MST")}
-				if f, index := utils.FindName(csv_dir, object_name); f {
-					err = utils.UpdateCSV(csv_dir, "update", index, record)
-					if err != nil {
-						http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-						return
-					}
-				} else {
-					err = utils.WriteCSV(csv_dir, record)
-					if err != nil {
-						http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-						return
-					}
-				}
-
-			w.WriteHeader(status)
-			w.Write([]byte("Object was added successfully!"))
 		}
+	}
+}
+
+func ObjectPut(w http.ResponseWriter, r *http.Request, dir, bucket_name, object_name string) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	object_dir := path.Join(dir, bucket_name, object_name)
+	status := object.PutObject(data, object_dir)
+	if status != http.StatusOK {
+		http.Error(w, http.StatusText(status), status)
+		return
+	}
+
+	csv_dir := path.Join(dir, bucket_name, "objects.csv")
+	record := []string{object_name, strconv.Itoa(int(r.ContentLength)), r.Header.Get("Content-Type"), time.Now().Format("2006-01-02 15:04:05 MST")}
+	if f, index := utils.FindName(csv_dir, object_name); f {
+		err = utils.UpdateCSV(csv_dir, "update", index, record)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		err = utils.WriteCSV(csv_dir, record)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.WriteHeader(status)
+	w.Write([]byte("Object was added successfully!"))
+}
+
+func ObjectGet(w http.ResponseWriter, r *http.Request, dir, bucket_name, object_name string) {
+	csv_dir := path.Join(dir, bucket_name, "objects.csv")
+	if f, _ := utils.FindName(csv_dir, object_name); f {
+		file, err := os.Open(path.Join(dir, bucket_name, object_name))
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		data, err := io.ReadAll(file)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		w.Write(data)
+	} else {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 	}
 }
