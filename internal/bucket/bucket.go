54e3c1fdf2332076048100bb778a949ba54e1ffd
ashpring <artshpringer2006@gmail.com>
2024-10-27 18:37:07 +0500

    done

--- internal/bucket/bucket.go
+++ internal/bucket/bucket.go
@@ -16,10 +16,13 @@ func CreateBucket(name string, dir string) (int, string) {
 
 	if flag, _, _ := utils.FindName(dir+"/buckets.csv", name); !flag {
 		bucket_dir := dir + "/" + name
-		os.Mkdir(bucket_dir, os.ModePerm)
+		err := os.Mkdir(bucket_dir, os.ModePerm)
+		if err != nil {
+			return http.StatusInternalServerError, "Internal Server Error"
+		}
 
 		metaData := []string{name, time.Now().Format("2006-01-02 15:04:05 MST"), time.Now().Format("2006-01-02 15:04:05 MST"), "InActive"}
-		err := utils.WriteCSV(dir+"/buckets.csv", metaData)
+		err = utils.WriteCSV(dir+"/buckets.csv", metaData)
 		if err != nil {
 			return http.StatusInternalServerError, "Internal Server Error"
 		}
